62cf41712ed3e18135ff1e5979fcb6e277dc4bdd
Ahmed Ragab <ahmedragab20901@gmail.com>
2022-05-16 21:46:49 +0200

    🐛 FIX:

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
     '~plugins/vuetify.js',
     '~plugins/client.vars.js',
     '~plugins/axios.js',
+    '~plugins/vue-carousel.js',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
